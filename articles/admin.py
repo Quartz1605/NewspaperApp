ee0ad652a449dbb3d155e0148529fcc5c6511201
Quartz1605 <prathambarekal05@gmail.com>
2025-04-18 16:27:32 +0530

    Finished the app

--- articles/admin.py
+++ articles/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article,Comment
 
 # Register your models here.
 
-class AdminView(admin.ModelAdmin):
+# This stuff here is to display your stuff better on the admin page
+
+#class CommentInline(admin.StackedInline):
+  #model = Comment
+
+class CommentInline(admin.TabularInline):
+  model = Comment
+  extra = 0
+
+#class AdminView(admin.ModelAdmin):
   
+  
+class ArticleAdmin(admin.ModelAdmin):
+  inlines = [
+    CommentInline,
+  ]
+
   list_display = [
     "title",
     "body",
@@ -13,5 +28,5 @@ class AdminView(admin.ModelAdmin):
 
 
 
-
-admin.site.register(Article,AdminView)
+admin.site.register(Article,ArticleAdmin)
+admin.site.register(Comment)
